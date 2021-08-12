d7e927924d48fa62497114f72c5a93b766102913
igotapps650 <igotapps650@gmail.com>
2021-08-11 18:17:53 -1200

    serving index.html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config({path: "./config.env"});
 const PORT = process.env.PORT;
 
@@ -10,6 +11,18 @@ app.get("/",(req,res)=>{
 })
 
  // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+
+ //set static assets if in production
+if (process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));  
+    })
+  
+}
+
 app.listen(PORT,(err)=>{
     if(err) return console.log(err);
     console.log("server is runing on port " + PORT)
